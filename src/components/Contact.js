eac371e61f98e5cba9eb7105453c274e6a4ec604
hyup <0906sgh@gmail.com>
2017-01-12 15:42:45 +0900

    final

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,7 +5,6 @@ import update from 'react-addons-update';
 import ContactCreate from './ContactCreate';
 
 export default class Contact extends React.Component {
- // 시작이다
   constructor(props) {
     super(props);
     this.state= {
